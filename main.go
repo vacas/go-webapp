58f2b8f9474b8507ee380837064b2c1ea6f3f66b
vacas <mfjvacas@gmail.com>
2019-01-21 18:22:26 -0500

    Initialized and starting up to chap 2.5 of Let's Go

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  "log"
+  "net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+  if r.URL.Path != "/" {
+    http.NotFound(w, r)
+    return
+  }
+
+  w.Write([]byte("Hello from this page"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Display a specific snippet..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+  if r.Method != "POST" {
+    w.Header().Set("Allow", "POST")
+    // w.WriteHeader(405)
+    w.Error(w, "Method Not Allowed", 405)
+    return
+  }
+
+  w.Write([]byte("Create a specific snippet..."))
+}
+
+func main() {
+  mux := http.NewServeMux()
+  mux.HandleFunc("/", home)
+  mux.HandleFunc("/snippet", showSnippet)
+  mux.HandleFunc("/snippet/create", createSnippet)
+
+  log.Println("Starting server on :4000")
+  err := http.ListenAndServe(":4000", mux)
+  log.Fatal(err)
+}
